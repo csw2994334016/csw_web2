2e4382e3c5f8490f9c70a5731b6cfdb001cd85e2
chennanjin <1553877174@qq.com>
2018-05-25 23:33:53 +0800

    加入angularJS请求

---
+++ static/js/common/http.js
@@ -0,0 +1,69 @@
+(function () {
+  angular.module('app').factory('$httpAjax', function () {
+      var server = {};
+      function init(){
+          server.data = null;
+          server.statusText = '';
+          server.code = '';
+          server.success = null;
+      }
+      // 处理请求成功
+      function handleSuccess (res, successCallback, errorCallback) {
+        if (res.code === '0000') {
+          successCallback(res)
+        } else {
+          res.message = res.message ? res.message : '网络繁忙';
+          errorCallback(res)
+        }
+      }
+      // 处理请求错误
+      function handleError(res, errorCallback) {
+        res.message = res.message ? res.message : '网络繁忙';
+        errorCallback(res);
+      }
+      // post请求
+      server.post = function(url,params,successCallback,errorCallback,completeBack) {
+        init()
+        $.ajax({
+          url: url,
+          method: 'POST',
+          data: params,
+          cache:false,
+          success:function (response) {
+            handleSuccess(response, successCallback, errorCallback)
+          },
+          error:function (response) {
+            handleError(response,errorCallback)
+          },
+          complete:function () {
+            completeBack && completeBack();
+            }
+        });
+      };
+      // get请求
+      server.get = function(url,params,successCallback,errorCallback,completeBack) {
+        init()
+        debugger
+        var headers = { 'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8"};
+        $.ajax({
+          url: url,
+          method: "GET",
+          headers:headers,
+          cache:true,
+          success:function (response) {
+            handleSuccess(response, successCallback, errorCallback)
+          },
+          error:function (response) {
+            handleError(response, errorCallback)
+          },
+          complete:function () {
+            completeBack && completeBack();
+            }
+        });
+      };
+      server.download = function (url) {
+        location.href = url;
+      };
+    return server;
+    })
+})();
--- static/js/pageJs/stock/stockQuery.js
+++ static/js/pageJs/stock/stockQuery.js
@@ -1,59 +1,83 @@
 (function () {
     var app = angular.module('app', []);
-    app.controller('stockInquiryCtrl', function($scope) {
+    app.controller('stockInquiryCtrl', function($scope, $httpAjax) {
 
-        $scope.size = []   //料号
-        $scope.cangqu = []  //仓区
-        $scope.chuqu = []  //储区
-        $scope.chuwei = []  //储位
+      $scope.size = []   //料号
+      $scope.cangqu = []  //仓区
+      $scope.chuqu = []  //储区
+      $scope.chuwei = []  //储位
+      $scope.data = []  // 表格数据
 
-        $scope.selectedSize = 1
-        $scope.selectedCangqu = 1
-        $scope.selectedChuqu = 1
-        $scope.selectedChuwei = 1
+      $scope.selectedSize = 1
+      $scope.selectedCangqu = 1
+      $scope.selectedChuqu = 1
+      $scope.selectedChuwei = 1
 
-        getMockData()
+      getMockData()
 
-        function getMockData(){
+      function getMockData() {
 
-            for (var index = 0; index< 5; index++) {
-                $scope.size.push({
-                    id: index+1,
-                    name: '料号'+ (index+1),
-                })
-            }
-            for (var index = 0; index< 2; index++) {
-                $scope.cangqu.push({
-                    id: index+1,
-                    name: '仓区'+ (index+1),
-                })
-            }
-            for (var index = 0; index< 6; index++) {
-                $scope.chuqu.push({
-                    id: index+1,
-                    name: '储区'+ (index+1),
-                })
-            }
-            for (var index = 0; index< 7; index++) {
-                $scope.chuwei.push({
-                    id: index+1,
-                    name: '储位'+ (index+1),
-                })
-            }
+        for (var index = 0; index < 5; index++) {
+          $scope.size.push({
+            id: index + 1,
+            name: '料号' + (index + 1),
+          })
         }
+        for (var index = 0; index < 2; index++) {
+          $scope.cangqu.push({
+            id: index + 1,
+            name: '仓区' + (index + 1),
+          })
+        }
+        for (var index = 0; index < 6; index++) {
+          $scope.chuqu.push({
+            id: index + 1,
+            name: '储区' + (index + 1),
+          })
+        }
+        for (var index = 0; index < 7; index++) {
+          $scope.chuwei.push({
+            id: index + 1,
+            name: '储位' + (index + 1),
+          })
+        }
+        for (var index = 0; index < 7; index++) {
+          $scope.data.push({
+            liaoHao: index + 1,
+            pinMing: '品名' + (index + 1),
+            xingHao: '',
+            zaiKu: 25,
+            pinZhi: '',
+            danWei: '件',
+            cangKu: 1,
+            cangQu: 1,
+            chuQu: 1,
+            chuWei: 1,
+          })
+        }
+      }
 
-        $scope.$watch('selectedSize', function () {
-            console.log($scope.selectedSize)
-        })
-        $scope.$watch('selectedCangqu', function () {
-            console.log($scope.selectedCangqu)
-        })
-        $scope.$watch('selectedChuqu', function () {
-            console.log($scope.selectedChuqu)
-        })
-        $scope.$watch('selectedChuwei', function () {
-            console.log($scope.selectedChuwei)
-        })
+      $scope.$watch('selectedSize', function () {
+        console.log($scope.selectedSize)
+      })
+      $scope.$watch('selectedCangqu', function () {
+        console.log($scope.selectedCangqu)
+      })
+      $scope.$watch('selectedChuqu', function () {
+        console.log($scope.selectedChuqu)
+      })
+      $scope.$watch('selectedChuwei', function () {
+        console.log($scope.selectedChuwei)
+      })
 
-    });
+      apiTest()
+      function apiTest() {
+        var roleApi = 'http://47.98.251.95:8080/api/sys/roles';
+        $httpAjax.get(roleApi,null,function (success) {
+          console.log(success)
+        },function (error) {
+
+        })
+      }
+    })
 })();
