96a6c0221280d3ba9760c8c0cbafad3a2e58f110
nanjin.chen <nanjin.chen@shiyin.com>
2018-05-26 17:47:01 +0800

    加入dialogModal指令

--- static/js/common/directives.js
+++ static/js/common/directives.js
@@ -11,7 +11,6 @@ angular.module('app')
         },
         link: function (scope, el, attr) {
             scope.$watch('showloading', function (value) {
-                console.log(value)
                 if (value) {
                     el.addClass('loading-data').removeClass('hide')
                 } else {
@@ -21,3 +20,25 @@ angular.module('app')
           }
         };
     })
+.directive("modalDialog", function () {
+    return {
+        restrict: 'E',
+        replace: true, // Replace with the template below
+        templateUrl: '/pages/dialog.html' ,
+        transclude: true, // we want to insert custom content inside the directive
+        scope: {
+            show: '='
+        },
+        link: function(scope, el, attrs) {
+            scope.dialogStyle = {};
+
+            if (attrs.width)
+                scope.dialogStyle.width = attrs.width;
+            if (attrs.height)
+                scope.dialogStyle.height = attrs.height;
+            scope.hideModal = function() {
+                scope.show = false;
+            };
+        },
+    };
+})
